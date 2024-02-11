a400466e355d4d6bb5ee5a29c359d8037eb769e9
Jimmyokok <878415277@qq.com>
2024-02-11 16:08:37 +0800

    1.4.2

--- obj/Debug/netstandard2.1/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace LandValueOverhaul
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "LandValueOverhaul";
-        public const string PLUGIN_NAME = "LandValueOverhaul";
-        public const string PLUGIN_VERSION = "1.4.2";
-    }
-}
